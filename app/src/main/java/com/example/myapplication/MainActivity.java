8877e8458aa87d4548cab2c6c1677b7749cc85fb
SayeeemAhmed <sayeemstudent@gmail.com>
2022-02-27 13:30:25 +0600

    Facebook

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webView= (WebView) findViewById(R.id.webid);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.facebook.com/");
+    }
+}
